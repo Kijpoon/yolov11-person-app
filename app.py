f0daf2149e368ba80509f66442b31170543df99f
Kijsapoon <korawith.kho@outlook.com>
2025-09-21 14:25:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ import numpy as np
 st. title (" YOLO Image Detection App :)")
 # Load YOLO model
 # model = YOLO (" runs / detect / train73 / weights / best .pt ")
-model = YOLO ("yolo11n.pt")
+model = YOLO("yolo11n.pt")
 # Upload image
-uploaded_image = st. file_uploader (" Upload an image (jpg , png)", type =["jpg"",jpeg ","png"])
+uploaded_image = st. file_uploader("Upload an image (jpg, png)", type =["jpg","jpeg","png"])
 
 if uploaded_image is not None :
   # Show original image
-  st.image(uploaded_image , caption =" Uploaded Image ", use_container_width = True)
+  st.image(uploaded_image, caption ="Uploaded Image", use_container_width = True)
   # Read image and convert to numpy array
   image = Image.open(uploaded_image)
   image_np = np.array(image)
@@ -23,13 +23,12 @@ if uploaded_image is not None :
   # Draw results on image
 
   result_image = results [0]. plot ()
-  st. image ( result_image , caption =" YOLO Detection Result ", use_container_width =
-  True )
+  st.image(result_image , caption ="YOLO Detection Result", use_container_width = True)
   st. success (" Detection completed !")
   # Extract detection results
-  boxes = results [0]. boxes
-  class_ids = boxes .cls.cpu (). numpy (). astype (int)
-  class_names = [ model . names [i] for i in class_ids ]
+  boxes = results[0].boxes
+  class_ids = boxes.cls.cpu().numpy().astype(int)
+  class_names = [model.names [i] for i in class_ids ]
   # Count people
-  person_count = class_names . count (" person ")
+  person_count = class_names.count("person")
   st. write (f" Number of people detected : **{ person_count }**")
